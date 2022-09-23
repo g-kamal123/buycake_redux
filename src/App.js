c06f11a97293a6485ac9832fcc444eba0e68de27
g-kamal123 <kamalnayanrai@cedcoss.com>
2022-09-23 11:49:52 +0530

    buycake

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { actions } from "./store/index";
+import buy_slice from "./store/Buy-slice";
 import "./App.css";
-import { numcake } from "./store/Buy-slice";
 function App() {
   const num = useSelector((state) => state.numberofcake);
   const dispatch = useDispatch();
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <h2>Initial Number of cake :{numcake.numberofcake}</h2>
+      <h2>Initial Number of cake :{buy_slice.getInitialState().numberofcake}</h2>
       <h2>Current number of cake: {num}</h2>
       <button onClick={btnHandler}>Buy cake</button>
       {num === 0 && <h2 style={{ color: "red" }}>Can't buy stock is over</h2>}
